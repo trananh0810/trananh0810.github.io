65dc2bcdd9cb8b56f26bc81a8afd5e03de4751d0
TranAnh <trananh081095@gmail.com>
2019-10-29 15:49:38 +0700

    update path home

--- themes/js/index.js
+++ themes/js/index.js
@@ -1,9 +1,12 @@
 $(function(){
+    //show menu
     showMenu();
     $(window).scroll(()=>{
         showMenu();
     });
     
+    //hiệu ứng đánh chữ
+    effectTyping();
 })
 
 //show menu
@@ -16,4 +19,26 @@ var showMenu = () =>{
         $('#div-menu').removeClass('div-menu-show');
     }
 
+}
+
+//hiệu ứng đánh chữ
+var effectTyping = () => {
+    var content = $('#div-home-body>div:nth-child(3)').text();
+    var contentLength = content.length;
+    var indexChar = 0;
+    var typingSpeed = 50;
+
+    $('#div-home-body>div:nth-child(3)').text("");
+
+    var intervalTyping = setInterval(() => {
+        indexChar++;
+
+        var subContent = content.substring(0,indexChar);
+
+        $('#div-home-body>div:nth-child(3)').text(subContent);
+
+        if (indexChar == contentLength){
+            clearInterval(intervalTyping);
+        }
+    }, typingSpeed);
 }
