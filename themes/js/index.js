dea996043d1958a0da64bca8b112c7baf8f99d39
TranAnh <trananh081095@gmail.com>
2019-11-03 21:32:02 +0700

    update các phần còn lại

--- themes/js/index.js
+++ themes/js/index.js
@@ -3,6 +3,7 @@ $(function() {
   showMenu();
   $(window).scroll(() => {
     showMenu();
+    activeMenu();
   });
 
   //hiệu ứng đánh chữ
@@ -10,11 +11,39 @@ $(function() {
 
   //run progress bar
   runProgressbar();
+
+  //run statistical
+  runStatistical();
+
+  //isotope event
+  $('.div-event-content').isotope({
+    itemSelector: '.div-event-item'
+  });
+
+  //fancybox
+  $('[data-fancybox').fancybox({
+    loop: true,
+    protect: true,
+    buttons: [
+      "zoom",
+      "share",
+      "slideShow",
+      "thumbs",
+      "close"
+    ],
+    animationEffect: "zoom-in-out",
+    zoomOpacity: "auto",
+    transitionEffect: "zoom-in-out",
+    beforeLoad: $.noop
+  });  
+    
+    //click menu
+    clickMenu();
 });
 
 //show menu
-var showMenu = () => {
-  var indexWindow = $(window).scrollTop();
+let showMenu = () => {
+  let indexWindow = $(window).scrollTop();
 
   if (indexWindow >= 100) {
     $("#div-menu").addClass("div-menu-show");
@@ -24,18 +53,18 @@ var showMenu = () => {
 };
 
 //hiệu ứng đánh chữ
-var effectTyping = () => {
-  var content = $("#div-home-body>div:nth-child(3)").text();
-  var contentLength = content.length;
-  var indexChar = 0;
-  var typingSpeed = 50;
+let effectTyping = () => {
+  let content = $("#div-home-body>div:nth-child(3)").text();
+  let contentLength = content.length;
+  let indexChar = 0;
+  let typingSpeed = 50;
 
   $("#div-home-body>div:nth-child(3)").text("");
 
-  var intervalTyping = setInterval(() => {
+  let intervalTyping = setInterval(() => {
     indexChar++;
 
-    var subContent = content.substring(0, indexChar);
+    let subContent = content.substring(0, indexChar);
 
     $("#div-home-body>div:nth-child(3)").text(subContent);
 
@@ -46,13 +75,13 @@ var effectTyping = () => {
 };
 
 //chạy progress bar
-var runProgressbar = () => {
-  var markJava = 60;
-  var markHtml = 50;
-  var markCss = 50;
-  var markPts = 50;
-  var markJquery = 50;
-  var markBootstrap = 50;
+let runProgressbar = () => {
+  let markJava = 60;
+  let markHtml = 50;
+  let markCss = 50;
+  let markPts = 50;
+  let markJquery = 50;
+  let markBootstrap = 50;
 
   runProgressbarItem("#pgb-java", markJava);
   runProgressbarItem("#pgb-html", markHtml);
@@ -62,8 +91,8 @@ var runProgressbar = () => {
   runProgressbarItem("#pgb-bootstrap", markBootstrap);
 };
 
-var runProgressbarItem = (selector, mark) => {
-  var markRun = 0;
+let runProgressbarItem = (selector, mark) => {
+  let markRun = 0;
   setInterval(() => {
     markRun++;
 
@@ -72,3 +101,85 @@ var runProgressbarItem = (selector, mark) => {
     }
   }, 10);
 };
+
+//chạy thống kê
+let runStatistical = () => {
+  let projectCompleted = 0;
+  let currentProject = 0;
+  let award = 10;
+
+  runStatisticalItem('#span-project-completed', projectCompleted);
+  runStatisticalItem('#span-award', award);
+  runStatisticalItem('#span-current-project', currentProject);
+}
+
+let runStatisticalItem = (selector, count) => {
+  let countRun = 0;
+  setInterval(() => {
+    countRun++;
+
+    if (countRun <= count) {
+      $(selector).text(countRun);
+    }
+  }, 100);
+}
+
+//click menu
+let clickMenu = () => {
+  $('.a-menu-home').click(() => {
+    $('html').animate({scrollTop: 0});
+  });
+
+  $('.a-menu-about').click(() => {
+    let top = $('#div-about').offset().top + 'px';
+    $('html').animate({scrollTop: top});
+  });
+
+  $('.a-menu-edu').click(() => {
+    let top = $('#div-education').offset().top + 'px';
+    $('html').animate({scrollTop: top});
+  });
+
+  $('.a-menu-experience').click(() => {
+    let top = $('#div-experience').offset().top + 'px';
+    $('html').animate({scrollTop: top});
+  });
+
+  $('.a-menu-event').click(() => {
+    let top = $('#div-event').offset().top + 'px';
+    $('html').animate({scrollTop: top});
+  });
+
+  $('.a-menu-feedback').click(() => {
+    let top = $('#div-feedback').offset().top + 'px';
+    $('html').animate({scrollTop: top});
+  });
+}
+
+//active menu
+let activeMenu = () => {
+  let indexWindow = $(window).scrollTop();
+
+
+  let topAbout = $('#div-about').offset().top - $('#div-about').height()/2;
+  let topEdu = $('#div-education').offset().top - $('#div-education').height()/2;
+  let topExperience = $('#div-experience').offset().top - $('#div-experience').height()/2;
+  let topEvent = $('#div-event').offset().top - $('#div-event').height()/2;
+  let topFeedback = $('#div-feedback').offset().top - $('#div-feedback').height()/2;
+
+  $('.a-menu-item').removeClass("a-menu-active");
+
+  if (indexWindow < topAbout) {
+    $(".a-menu-home").addClass("a-menu-active");
+  } else if (indexWindow < topEdu) {
+    $(".a-menu-about").addClass("a-menu-active");
+  } else if (indexWindow < topExperience) {
+    $(".a-menu-edu").addClass("a-menu-active");
+  } else if (indexWindow < topEvent) {
+    $(".a-menu-experience").addClass("a-menu-active");
+  } else if (indexWindow < topFeedback) {
+    $(".a-menu-event").addClass("a-menu-active");
+  } else if (indexWindow >= topFeedback) {
+    $(".a-menu-feedback").addClass("a-menu-active");
+  }
+}
